30e59de513a0a4ff55e6c50e946bdfbde5868187
Dotsially <mateu9999@gmail.com>
2024-10-18 10:15:08 +0200

    Added SoundEffectManager and a Timer class. Added reload time.

--- Project/Assets/Scripts/Recoil.cs
+++ Project/Assets/Scripts/Recoil.cs
@@ -12,10 +12,23 @@ public class Recoil : MonoBehaviour
     public Transform playerSpine;
     public int explosionForce = 0;
     public int explosionRadius = 0;
+    public float reloadTime = 0.0f;
+    private Timer reloadTimer = new Timer();
 
 
+    public void Start(){
+    }
 
     void Update(){
+        reloadTimer.Tick();
+
+        if(Input.GetMouseButton(0) && reloadTimer.IsFinished()){
+            playerModel.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, explosionPosition.position, explosionRadius);
+            reloadTimer.Start(reloadTime);
+            //No indicator of shooting so
+            Debug.Log("Shot");
+        }
+
         playerModel.transform.rotation = Quaternion.Euler(0, cameraFollowTarget.transform.rotation.eulerAngles.y, 0);
         Quaternion lookRotation = Quaternion.LookRotation((mainCamera.position + mainCamera.forward * 10.0f) - playerSpine.position);
         playerSpine.rotation = lookRotation;
@@ -24,8 +37,9 @@ public class Recoil : MonoBehaviour
     
     void FixedUpdate()
     {
-        if(Input.GetMouseButton(0)){
-            playerModel.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, explosionPosition.position, explosionRadius);
-        }
+        /**
+            Might be necessary for the rigidbody physics I just didn't want to put the timer ticker here 
+            because it might cause some weird stuff to happen. This ticks at a solid 60 fps while update goes much faster.
+        **/
     }
 }
