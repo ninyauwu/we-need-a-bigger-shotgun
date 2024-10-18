eb941828744aef1ffedfe75d0ec88d3a236ee09b
IcyKoala <90623742+IcyKoala@users.noreply.github.com>
2024-10-18 11:46:21 +0200

    Modified the PauseMenu based on feedback
    - Moved the UI Manager script to make the prefab self-contained
    - Removed the now unneeded GameManager
    - Added summaries to public functions
    - changed variable namings

--- Project/Assets/Scripts/Managers/UIManager.cs
+++ Project/Assets/Scripts/Managers/UIManager.cs
@@ -4,43 +4,61 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject pauseMenu;
-    public GameObject settingsMenu;
+    public GameObject PauseMenu;
+    public GameObject SettingsMenu;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    /// <summary>
+    /// Called whenever the on-screen pause button is pressed
+    /// </summary>
     public void openPauseMenu() {
-        pauseMenu.SetActive(true);
+        PauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
 
+    /// <summary>
+    /// Called whenever the "Resume Game" button is pressed within the pause menu
+    /// </summary>
     public void closePauseMenu() {
-        pauseMenu.SetActive(false);
+        PauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
 
+    /// <summary>
+    /// Called whenever the "Settings" button is pressed within the pause menu
+    /// </summary>
     public void openSettings() {
-        pauseMenu.SetActive(false);
-        settingsMenu.SetActive(true);
+        PauseMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
     }
 
+    /// <summary>
+    /// Called whenever the "Return" button is pressed within the settings menu
+    /// </summary>
     public void closeSettings() {
-        settingsMenu.SetActive(false);
-        pauseMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+        PauseMenu.SetActive(true);
     }
 
+    /// <summary>
+    /// Called whenever the "Return to menu" button is pressed within the pause menu
+    /// </summary>
     public void returnToMenu() {
         // Add implementation
     }
 
+    /// <summary>
+    /// Called whenever the "Volume" slider is adjusted within the pause menu
+    /// </summary>
+    /// <param name="volumeValue">New desired volume level on a scale of 0-100</param>
     public void changeVolume(float volumeValue) {
         // Add implementation
     }
 
+    /// <summary>
+    /// Called whenever the "SFX" slider is adjusted within the pause menu
+    /// </summary>
+    /// <param name="sfxValue">New desired Sound effects volume level on a scale of 0-100</param>
     public void changeSFXVolume(float sfxValue) {
         // Add implementation
     }
